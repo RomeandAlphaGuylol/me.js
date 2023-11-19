7c66e9483c489849d0dad114955cd0b4f7872fa5
RomeandAlphaGuylol <lunaruser9@gmail.com>
2023-11-19 16:07:54 +0530

    Fixed Dead URL

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,64 +1,44 @@
-import Image from 'next/image'
-import { Inter } from 'next/font/google'
+import Image from 'next/image';
+import { Inter } from 'next/font/google';
 
-const inter = Inter({ subsets: ['latin'] })
+const inter = Inter({ subsets: ['latin'] });
 
 export default function Home() {
-  <title>Home - Rome</title>
   return (
-    
-    <main
-      className={`flex min-h-screen flex-col items-center justify-between p-24 ${inter.className}`}
-    >
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          
-          <code className="font-mono font-bold">Hello, World!</code>
-        </p>
-        
-      </div>
-{/* Made using NEXT.js React Framework, https://nextjs.org/*/}
-      <img className="logo" src="/logo.png"></img>
-      <div className="abme text-center text-x1 font-semibold text-purple-600">
-      </div>
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-
-        <a
-          href="https://github.com/RomeandAlphaGuylol/"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Github{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Check my repos!
+    <div className={`bg-gray-900 text-white min-h-screen py-12 px-4 ${inter.className}`}>
+      <title>Home - Rome</title>
+      <main className="container mx-auto max-w-4xl">
+        <div className="text-center">
+          <h1 className="text-4xl font-bold text-purple-400 mb-4">
+            Oi'! Welcome
+          </h1>
+          <p className="text-lg text-gray-400 mb-8">
+            Uhh
           </p>
-        </a>
+        </div>
 
-        
+        <div className="flex flex-wrap gap-6 justify-center">
+          <a
+            href="https://github.com/jshbb"
+            className="rounded-lg bg-purple-400 text-gray-900 hover:bg-purple-500 hover:text-white transition duration-300 ease-in-out p-4 shadow-lg text-center w-64"
+          >
+            <h2 className="text-xl font-semibold mb-2">Github</h2>
+            <p className="text-gray-900">
+              Explore my web development projects and contributions on GitHub.
+            </p>
 
-        <a
-          href="/connect"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Connections{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Connect with me!
-          </p>
-        </a>
-      </div>
-    </main>
-  )
+          </a>
+          <a
+            href="/connect"
+            className="rounded-lg bg-purple-400 text-gray-900 hover:bg-purple-500 hover:text-white transition duration-300 ease-in-out p-4 shadow-lg text-center w-64"
+          >
+            <h2 className="text-xl font-semibold mb-2">Contact</h2>
+            <p className="text-gray-900">
+              Let's connect! Reach out to me via some social medias.
+            </p>
+          </a>
+        </div>
+      </main>
+    </div>
+  );
 }
